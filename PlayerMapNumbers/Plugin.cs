a7eef0dd67e590cd1ce34f374e4ca1e6d0d627f6
Daniel Sloggett <48067784+dslogget@users.noreply.github.com>
2023-12-18 22:04:17 +0000

    add ability to switch by number

--- PlayerMapNumbers/Plugin.cs
+++ PlayerMapNumbers/Plugin.cs
@@ -9,6 +9,7 @@ using Unity.Netcode;
 using System.Text.RegularExpressions;
 using System.Reflection;
 using BepInEx.Logging;
+using System.Text;
 
 namespace PlayerMapNumbers
 {
@@ -62,32 +63,99 @@ namespace PlayerMapNumbers
             textRef.text = ( 1 + number ).ToString();
         }
 
-    }
-
-    [HarmonyPatch(typeof(ManualCameraRenderer), "Awake")]
-    public static class ManualCameraRendererPatch
-    {
-        public static void Postfix(ManualCameraRenderer __instance)
+        static public void UpdateNumbers()
         {
-            Plugin.StaticLogger.LogInfo("Postfix patch run");
-            NetworkManager networkManager = __instance.NetworkManager;
-            if ((UnityEngine.Object)networkManager == (UnityEngine.Object)null || !networkManager.IsListening)
-                return;
-            if ( StartOfRound.Instance?.mapScreen == null )
+            if (StartOfRound.Instance?.mapScreen == null)
             {
                 return;
             }
-            for (int index = 0; index < StartOfRound.Instance.allPlayerScripts.Length; ++index)
-            {      
-                if ( index > StartOfRound.Instance.mapScreen.radarTargets.Count )
-                {
-                    break;
-                }
+            for (int index = 0; index < StartOfRound.Instance.mapScreen.radarTargets.Count; ++index)
+            {
                 var transAndName = StartOfRound.Instance.mapScreen.radarTargets[index];
-                if (transAndName.transform != null) {
-                    Plugin.AddPlayerNumber(transAndName.transform.gameObject, index);
+                if (transAndName.transform != null)
+                {
+                    AddPlayerNumber(transAndName.transform.gameObject, index);
+                }
+            }
+        }
+
+    }
+
+    [HarmonyPatch(typeof(ManualCameraRenderer), "Awake")]
+    public static class ManualCameraRendererAwakePatch
+    {
+        public static void Postfix(ManualCameraRenderer __instance)
+        {
+            Plugin.StaticLogger.LogInfo("ManualCameraRendererAwakePatch patch run");
+            NetworkManager networkManager = __instance.NetworkManager;
+            if ((UnityEngine.Object)networkManager == (UnityEngine.Object)null || !networkManager.IsListening)
+                return;
+            Plugin.UpdateNumbers();
+        }
+    }
+
+    [HarmonyPatch(typeof(ManualCameraRenderer), "RemoveTargetFromRadar")]
+    public static class ManualCameraRendererRemoveTargetFromRadarPatch
+    {
+        public static void Postfix(ManualCameraRenderer __instance, Transform removeTransform)
+        {
+            Plugin.StaticLogger.LogInfo("ManualCameraRendererRemoveTargetFromRadarPatch patch run");
+            Plugin.UpdateNumbers();
+        }
+    }
+
+    [HarmonyPatch(typeof(ManualCameraRenderer), "AddTransformAsTargetToRadar")]
+    public static class ManualCameraRendererAddTransformAsTargetToRadarPatch
+    {
+        public static void Postfix(ManualCameraRenderer __instance, Transform newTargetTransform, string targetName, bool isNonPlayer)
+        {
+            Plugin.StaticLogger.LogInfo("ManualCameraRendererAddTransformAsTargetToRadar patch run");
+            Plugin.UpdateNumbers();
+        }
+    }
+
+    [HarmonyPatch(typeof(Terminal), "ParsePlayerSentence")]
+    public static class TerminalParsePlayerSentencePatch
+    {
+        static private string RemovePunctuation(string s)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (!char.IsPunctuation(c))
+                    stringBuilder.Append(c);
+            }
+            return stringBuilder.ToString().ToLower();
+        }
+        public static void Postfix(Terminal __instance, ref TerminalNode __result)
+        {
+            Plugin.StaticLogger.LogInfo("TerminalParsePlayerSentence patch run");
+            // 10, 11, 12 parse errors
+            if (__result == __instance.terminalNodes.specialNodes[10]) {
+                Plugin.StaticLogger.LogInfo("Extended Parse");
+                string str1 = RemovePunctuation(__instance.screenText.text.Substring(__instance.screenText.text.Length - __instance.textAdded));
+                string[] strArray = str1.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int outputNum;
+                if ( strArray.Length == 1 && int.TryParse( strArray[0], out outputNum ) )
+                {
+                    Plugin.StaticLogger.LogInfo("Number Found");
+                    int playerIndex = outputNum - 1;
+                    if ( playerIndex < StartOfRound.Instance.mapScreen.radarTargets.Count )
+                    {
+                        Plugin.StaticLogger.LogInfo("Valid Number");
+                        var controller = StartOfRound.Instance.mapScreen.radarTargets[playerIndex].transform.gameObject.GetComponent<PlayerControllerB>();
+                        if ( controller != null &&
+                             !controller.isPlayerControlled && !controller.isPlayerDead && controller.redirectToEnemy == null )
+                        {
+                            return;
+                        }
+                        StartOfRound.Instance.mapScreen.SwitchRadarTargetAndSync(playerIndex);
+                        Plugin.StaticLogger.LogInfo("Updated Target");
+                        __result = __instance.terminalNodes.specialNodes[20];
+                    }
                 }
             }
         }
     }
+
 }
--- PlayerMapNumbers/obj/Debug/netstandard2.1/PlayerMapNumbers.AssemblyInfo.cs
+++ PlayerMapNumbers/obj/Debug/netstandard2.1/PlayerMapNumbers.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Adds player map numbers")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1+61b63c84a9d22ca8d22329870050ceee4f48f658")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayerMapNumbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayerMapNumbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.1.0")]
