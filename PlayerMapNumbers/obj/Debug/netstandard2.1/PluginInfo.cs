34a763a638aa7d7906f0850042441e369f6b7b87
Daniel Sloggett <48067784+dslogget@users.noreply.github.com>
2023-12-19 00:56:20 +0000

    gitignore

--- PlayerMapNumbers/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace PlayerMapNumbers
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "PlayerMapNumbers";
-        public const string PLUGIN_NAME = "PlayerMapNumbers";
-        public const string PLUGIN_VERSION = "0.0.1";
-    }
-}
